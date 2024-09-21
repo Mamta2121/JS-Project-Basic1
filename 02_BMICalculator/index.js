5b69295bad1903c945eeb6c2f24f131a8d4344fc
Mamta2121 <mamtarajera2121@gmail.com>
2024-09-21 06:57:13 +0000

    container config done

---
+++ 02_BMICalculator/index.js
@@ -0,0 +1,48 @@
+ const form = document.querySelector('form')
+
+//this usecase will give you empty
+
+/*
+  const height = parseInt(document.querySelector('#height').value)
+    const weight= parseInt(document.querySelector('#weight').value)
+*/
+ form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
+    const result = document.querySelector('#results')
+
+
+    if(height == '' || height < 0 || isNaN(height))
+    {
+        result.innerHTML = "Please give a Valid height"
+    }
+    else if(weight == '' || weight < 0 || isNaN(weight))
+    {
+        result.innerHTML = "Please give a Valid weight"
+    }
+    else 
+    {
+           const bmi = (weight / ((height*height)/10000)).toFixed(2)
+           result.innerHTML =  `<span>Your BMI is ${bmi}</span>`
+
+           if(bmi < 18.6)
+           {
+              document.querySelector('#firstCat').textContent = 'YES';
+           }
+           else if(bmi > 18.6 && bmi < 24.9)
+           {
+            document.querySelector('#secondCat').textContent = 'YES';
+                
+           }
+           else
+           {
+            document.querySelector('#thirdCat').textContent = 'YES';
+           }
+           
+    }
+
+    
+    
+ })
